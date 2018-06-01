f31b82d50c4fa8cc4b3aad07b1ca018bcb112413
Charpell <charpellumeh@gmail.com>
2018-06-01 21:01:36 +0100

    Setup middlewares, constants and database enviroment

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import express from 'express';
 
+import constants from './config/constants';
+import './config/database';
+import middlewareConfig from './config/middlewares';
+
+console.log('======================');
+console.log(constants);
+console.log('======================');
+
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+middlewareConfig(app);
 
-app.listen(PORT, err => {
-  if (err) { 
+app.get('/', (req, res) => {
+  res.send('Hello world');
+})
+
+app.listen(constants.PORT, err => {
+  if (err) {
     throw err;
   } else {
     console.log(`
-      Server running on port: ${PORT}
+      Server running on port: ${constants.PORT}
       ----
       Running on ${process.env.NODE_ENV}
       ----
